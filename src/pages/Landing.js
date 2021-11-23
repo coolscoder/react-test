c00bccf259dcf3d1d76d4e0a9a801ae178a4662c
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-24 06:03:38 +0900

    feat: clean up code bases

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,11 +6,11 @@ const Landing = () => {
   return (
     <div>
       <div className="logo-container">
-        <img className={classNames('border mt-5 me-5 landing landing-1')} src="./art.jpg" />
-        <img className={classNames('border me-5 landing')} src="./art.jpg" />
+        <img className={classNames('border mt-5 landing')} src="./art.jpg" />
+        <img className={classNames('border landing landing-img')} src="./art.jpg" />
         <img className={classNames('border mt-5 landing')} src="./art.jpg" />
       </div>
-      <img className="mega" src="./mega.jpg" />
+      <img className="mega" src="./mega.png" />
       <p className="title">ACHIEVEMENT UNLOCKED</p>
       <p className="mb-5 description">Your horde has risen to the challenge and won! We've added your reward to your wallet</p>
       <Footer />
