d0a2bc0b9366ac4f62fcb1e211d38c3fb5effba3
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-24 04:40:38 +0900

    feat: clean up code bases

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
+import classNames from 'classnames';
 import './App.css';
 
 const App = () => {
-  const [animate, setAnimate] = useState(false)
+  const [animate1, setAnimate1] = useState(false)
+  const [animate2, setAnimate2] = useState(false)
+  const [animate3, setAnimate3] = useState(false)
 
   const handleStart = () => {
-    setAnimate(true)
+    setAnimate1(true)
+    setTimeout(() => {setAnimate2(true)}, 1000)
+    setTimeout(() => {setAnimate3(true)}, 2000)
   }
 
   return (
     <div className="App">
-      <div className="d-flex mb-5">
-        <img className="border me-3 art art-1" src="./art.jpg" />
-        <img className="border me-3 art art-2" src="./art.jpg" />
-        <img className="border me-3 art art-3" src="./art.jpg" />
-      </div>
-      {!animate && <Button onClick={handleStart}>Send My Team</Button>}
+      {!animate3 && <div className="d-flex mb-5">
+        <img className={classNames('border me-3 art', animate1 && 'art-ani')} src="./art.jpg" />
+        <img className={classNames('border me-3 art', animate2 && 'art-ani')} src="./art.jpg" />
+        <img className={classNames('border me-3 art', animate3 && 'art-ani')} src="./art.jpg" />
+      </div>}
+      {!animate1 && <Button variant="outline-primary" onClick={handleStart}>Send My Team</Button>}
     </div>
   );
 }
