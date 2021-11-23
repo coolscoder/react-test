d102c88e78363c8b5527c4b14f11df499a1cadf6
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-24 04:42:58 +0900

    feat: clean up code bases

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ const App = () => {
   const [animate1, setAnimate1] = useState(false)
   const [animate2, setAnimate2] = useState(false)
   const [animate3, setAnimate3] = useState(false)
+  const [showLogo, setShowLogo] = useState(true)
 
   const handleStart = () => {
     setAnimate1(true)
     setTimeout(() => {setAnimate2(true)}, 1000)
     setTimeout(() => {setAnimate3(true)}, 2000)
+    setTimeout(() => {setShowLogo(false)}, 4000)
   }
 
   return (
     <div className="App">
-      {!animate3 && <div className="d-flex mb-5">
+      {showLogo && <div className="d-flex mb-5">
         <img className={classNames('border me-3 art', animate1 && 'art-ani')} src="./art.jpg" />
         <img className={classNames('border me-3 art', animate2 && 'art-ani')} src="./art.jpg" />
         <img className={classNames('border me-3 art', animate3 && 'art-ani')} src="./art.jpg" />
